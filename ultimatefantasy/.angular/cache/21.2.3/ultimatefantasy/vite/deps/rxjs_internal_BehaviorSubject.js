143fecb7ee7301c1ab6eadead1f628e174dbf69d
CadenEbert <116670041+CadenEbert@users.noreply.github.com>
2026-05-24 19:18:00 -0500

    fix: untrack .angular cache

--- ultimatefantasy/.angular/cache/21.2.3/ultimatefantasy/vite/deps/rxjs_internal_BehaviorSubject.js
+++
@@ -1,9 +0,0 @@
-import {
-  BehaviorSubject
-} from "./chunk-YEDPF7GE.js";
-import "./chunk-P23EXAFI.js";
-import "./chunk-B2KI3AIV.js";
-import "./chunk-H2SRQSE4.js";
-export {
-  BehaviorSubject
-};
--- ultimatefantasy/.angular/cache/21.2.3/ultimatefantasy/vite/deps_ssr/rxjs_internal_operators_switchMap.js
+++
@@ -1,7 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import {
-  require_switchMap
-} from "./chunk-CNAPUAHZ.js";
-import "./chunk-KCMVP6QR.js";
-import "./chunk-6DU2HRTW.js";
-export default require_switchMap();
